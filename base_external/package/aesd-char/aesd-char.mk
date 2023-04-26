42b177576ede9e451b5180d0acf4d591eb4bf589
ifuterman <ifutermang@gmail.com>
2023-04-27 02:40:49 +0300

    completed

--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_VERSION = '7ecc9683479c06ab7fd568bec9cd11c26ba5c118'
+AESD_CHAR_VERSION = '9c5247ad421aa1ab7196b46699ccfd558d5e636b'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
